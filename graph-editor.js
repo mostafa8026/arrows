93d1dd40ed184427cdb9810d82faba238debff4d
mostafa H <mostafaheidary@hotmail.com>
2020-06-19 15:44:53 +0430

    fix: add utf8-support.

--- graph-editor.js
+++ graph-editor.js
@@ -365,7 +365,7 @@ window.onload = function()
 
     function updateSvgDownloadLink() {
       var rawSvg = new XMLSerializer().serializeToString(d3.select("#canvas svg" ).node());
-      d3.select("#downloadSvgButton").attr('href', "data:image/svg+xml;base64," + btoa( rawSvg ));
+      d3.select("#downloadSvgButton").attr('href', "data:image/svg+xml;base64," + btoa(unescape(encodeURIComponent( rawSvg ))));
     }
 
     var openConsoleWithCypher = function (evt)
